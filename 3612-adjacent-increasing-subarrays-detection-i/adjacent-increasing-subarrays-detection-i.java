1609367cbb52f1ebf5dda93d886c0fe90d9f944b
Jugraunaq Singh <116112286+Jugraunaqsingh@users.noreply.github.com>
2025-10-14 12:44:54 +0530

    Time: 2 ms (47.79%) | Memory: 44.4 MB (67.65%) - LeetSync

---
+++ 3612-adjacent-increasing-subarrays-detection-i/adjacent-increasing-subarrays-detection-i.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean hasIncreasingSubarrays(List<Integer> nums, int k) {
+        int n = nums.size();
+        for (int i = 0; i <= n - 2 * k; i++) {
+            boolean first = true;
+            for (int j = i; j < i + k - 1; j++) {
+                if (nums.get(j) >= nums.get(j + 1)) {
+                    first = false;
+                    break;
+                }
+            }
+            boolean second = true;
+            for (int j = i + k; j < i + 2 * k - 1; j++) {
+                if (nums.get(j) >= nums.get(j + 1)) {
+                    second = false;
+                    break;
+                }
+            }
+            if (first && second) return true;
+        }
+        return false;
+    }
+}
